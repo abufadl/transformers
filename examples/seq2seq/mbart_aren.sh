d2bcd55982e3def09880125ac352fff3d9828785
abufadl <61349083+abufadl@users.noreply.github.com>
2020-09-06 10:00:10 +0300

    Create mbart_aren.sh

---
+++ examples/seq2seq/mbart_aren.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+export PYTHONPATH="../":"${PYTHONPATH}"
+
+python finetune.py \
+    --learning_rate=3e-5 \
+    --gpu 1 \
+    --fp16 \
+    --do_train \
+    --val_check_interval=0.25 \
+    --adam_eps 1e-06 \
+    --num_train_epochs 1 --src_lang ar_AR --tgt_lang en_XX \
+    --data_dir $ENAR_DIR \
+    --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
+    --train_batch_size=$BS --eval_batch_size=$BS \
+    --task translation \
+    --warmup_steps 500 \
+    --freeze_embeds \
+    --model_name_or_path=facebook/mbart-large-cc25 \
+    --output_dir /kaggle/aren_finetune_baseline \
+    --label_smoothing 0.1 \
+    --fp16_opt_level=O1 --sortish_sampler --n_train 1000 --n_val 500 \
+    "$@"
